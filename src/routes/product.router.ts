be3a0a66acccce19fa37de408a46a4493691dac5
Carlos Bilby <carlos.bilby@gmail.com>
2022-08-20 20:44:56 -0300

    style: Formatting changes on routes due to eslint pattern

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import ProductController from '../app/controller/ProductController';
-import createValidation from '../app/validations/product/create';
-import putValidation from '../app/validations/product/put';
-import patchValidation from '../app/validations/product/patch';
+import { Router } from 'express'
+import ProductController from '../app/controller/ProductController'
+import createValidation from '../app/validations/product/create'
+import putValidation from '../app/validations/product/put'
+import patchValidation from '../app/validations/product/patch'
 
-const router = Router();
+const router = Router()
 
-router.post('/api/v1/product', createValidation, ProductController.create);
-router.get('/api/v1/product/:id', ProductController.getOne);
-router.get('/api/v1/product/low_stock', ProductController.getLowStock);
-router.get('/api/v1/product', ProductController.get);
-router.put('/api/v1/product/:id', putValidation, ProductController.update);
+router.post('/api/v1/product', createValidation, ProductController.create)
+router.get('/api/v1/product/:id', ProductController.getOne)
+router.get('/api/v1/product/low_stock', ProductController.getLowStock)
+router.get('/api/v1/product', ProductController.get)
+router.put('/api/v1/product/:id', putValidation, ProductController.update)
 router.patch('/api/v1/product/:id', patchValidation, ProductController.update)
 
-export default router;
+export default router
