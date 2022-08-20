8ae130df9833bb0d56e1da84f76cc9f38e59edee
Carlos Bilby <carlos.bilby@gmail.com>
2022-08-20 20:47:37 -0300

    refactor: Removed unneeded ternary operator

--- src/app/service/ProductService.ts
+++ src/app/service/ProductService.ts
@@ -1,42 +1,42 @@
-import { IProductResponse, IProduct } from '../interfaces/IProduct';
-import ProductRepository from '../repository/ProductRepository';
+import { IProductResponse, IProduct } from '../interfaces/IProduct'
+import ProductRepository from '../repository/ProductRepository'
 
 class ProductService {
-  async create(payload: IProduct): Promise<IProductResponse> {
-    const result = await ProductRepository.create(payload);
-    return result;
+  async create (payload: IProduct): Promise<IProductResponse> {
+    const result = await ProductRepository.create(payload)
+    return result
   }
 
-  async update(payload: IProduct, productId: string): Promise<IProductResponse | null> {
-    payload.stock_control_enabled = payload.qtd_stock > 0 ? true : false;
-    payload.updatedAt = new Date();
+  async update (payload: IProduct, productId: string): Promise<IProductResponse | null> {
+    payload.stock_control_enabled = payload.qtd_stock > 0
+    payload.updatedAt = new Date()
 
-    const result = await ProductRepository.update(payload, productId);
-    return result;
-  }  
-  
-  async patch(payload: IProduct, productId: string): Promise<IProductResponse | null> {
-    payload.stock_control_enabled = payload.qtd_stock > 0 ? true : false;
-    payload.updatedAt = new Date();
-
-    const result = await ProductRepository.patch(payload, productId);
-    return result;
+    const result = await ProductRepository.update(payload, productId)
+    return result
   }
 
-  async get(payload: IProduct) {
-    const result = await ProductRepository.get(payload);
-    return result;
+  async patch (payload: IProduct, productId: string): Promise<IProductResponse | null> {
+    payload.stock_control_enabled = payload.qtd_stock > 0
+    payload.updatedAt = new Date()
+
+    const result = await ProductRepository.patch(payload, productId)
+    return result
   }
 
-  async getLowStock() {
-    const result = await ProductRepository.getLowStock();
-    return result;
+  async get (payload: IProduct) {
+    const result = await ProductRepository.get(payload)
+    return result
   }
 
-  async getOne(productId: string) {
-    const result = await ProductRepository.getOne(productId);
-    return result;
+  async getLowStock () {
+    const result = await ProductRepository.getLowStock()
+    return result
+  }
+
+  async getOne (productId: string) {
+    const result = await ProductRepository.getOne(productId)
+    return result
   }
 }
 
-export default new ProductService();
+export default new ProductService()
