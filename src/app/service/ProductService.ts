9e2f22df2b5a06faafe495fe8f7d066d9bfd0ef4
Carlos Bilby <carlos.bilby@gmail.com>
2022-08-23 22:17:20 -0300

    feat: createCSV function created on product service

--- src/app/service/ProductService.ts
+++ src/app/service/ProductService.ts
@@ -1,12 +1,15 @@
+import { Readable } from 'stream';
 import BadRequestError from '../errors/BadRequestError'
 import { Types } from 'mongoose'
 import { IProductResponse, IProduct } from '../interfaces/IProduct'
 import ProductRepository from '../repository/ProductRepository'
 import NotFoundError from '../errors/NotFoundError'
+import readline from 'readline'
 
 
 class ProductService {
   async create (payload: IProduct): Promise<IProductResponse> {
+
     const result = await ProductRepository.create(payload)
     return result
   }
@@ -73,6 +76,43 @@ class ProductService {
 
     const result = await ProductRepository.delete(productId)
     return result
+  }
+
+  ///////////////////
+  async createCSV (file: any): Promise<IProduct> {
+    
+    const { buffer } = file
+
+    const readableFile = new Readable();
+    readableFile.push(buffer)
+    readableFile.push(null)
+
+    const producstLine = readline.createInterface({
+    input: readableFile
+    })
+
+    const products: any[] = []
+
+    for await(let line of producstLine) {
+      const productLineSplit = line.split(",")
+      console.log(productLineSplit[1])
+        
+      products.push({ 
+        title: productLineSplit[0],
+        description: productLineSplit[1],
+        department: productLineSplit[2],
+        brand: productLineSplit[3],
+        price: Number(productLineSplit[4]),
+        qtd_stock: Number(productLineSplit[5]),
+        bar_codes: productLineSplit[6]
+
+      })
+    }
+
+    //console.log(products)
+
+    const result = await ProductRepository.createCSV(products)
+    return result;
   }
 }
 
