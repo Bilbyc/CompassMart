46614404665afd0c2c673e5347a10ae4287c4f09
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-01 00:56:41 -0300

    feat: Create user entity interface

---
+++ src/app/interfaces/iUser.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  email: string;
+  password: string;
+  habilitated: boolean;
+
+}
+
+export interface IUserResponse {
+  email: string;
+  password: string;
+  habilitated: boolean;
+
+}
