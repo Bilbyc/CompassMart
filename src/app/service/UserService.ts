086c3f59679211126720671dec323667380435a2
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-01 01:02:38 -0300

    feat: Add create function on user service for Post req

--- src/app/service/UserService.ts
+++ src/app/service/UserService.ts
@@ -1,5 +1,11 @@
-class UserService {
+import { IUserResponse } from './../interfaces/iUser'
+import UserRepository from '../repository/UserRepository'
+import { IUser } from '../interfaces/IUser'
 
+class UserService {
+  async create (payload: IUser): Promise<IUserResponse> {
+    return UserRepository.create(payload)
+  }
 }
 
 export default new UserService()
