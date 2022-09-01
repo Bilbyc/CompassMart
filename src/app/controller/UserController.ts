a01f1405e16e2e1959a9b007e19b364e100a8276
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-01 01:04:45 -0300

    feat: Add create function on controller for Post req method

--- src/app/controller/UserController.ts
+++ src/app/controller/UserController.ts
@@ -1,5 +1,22 @@
-class UserController {
+import { IUser, IUserResponse } from '../interfaces/IUser'
+import UserService from '../service/UserService'
 
+class UserController {
+  async create (req, res): Promise<IUserResponse> {
+    try {
+      const payload: IUser = req.body
+      const result = await UserService.create(payload)
+
+      return res.status(201).json(result)
+    } catch (error) {
+      return res.status(error.statusCode || 500).json({
+        message: error.name,
+        details: [
+          { message: error.message }
+        ]
+      })
+    }
+  }
 }
 
 export default new UserController()
