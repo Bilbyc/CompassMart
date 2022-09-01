06763a16ae4078694858bee9db9482f90dd21940
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-01 01:01:04 -0300

    feat: Add create function on user repository

--- src/app/repository/UserRepository.ts
+++ src/app/repository/UserRepository.ts
@@ -1,5 +1,10 @@
-class UserRepository {
+import UserSchema from '../schema/UserSchema'
+import { IUser } from '../interfaces/IUser'
 
+class UserRepository {
+  async create (payload: IUser): Promise<IUser> {
+    return UserSchema.create(payload)
+  }
 }
 
 export default new UserRepository()
