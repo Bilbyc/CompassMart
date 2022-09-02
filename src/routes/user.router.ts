319d8e594babe6bd30e864cd31d1084eade84dd5
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-02 11:18:43 -0300

    feat: Added authenticate route to User routes

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -5,6 +5,6 @@ import createValidation from '../app/validations/user/create'
 const router = Router()
 
 router.post('/api/v1/user', createValidation, UserController.create)
-// router.post('/api/v1/authenticate', createValidation, UserController.authenticate)
+router.post('/api/v1/authenticate', createValidation, UserController.authenticate)
 
 export default router
