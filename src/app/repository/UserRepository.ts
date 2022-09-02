54b59980a498355a093bcf040b58ead66d23742a
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-02 11:08:25 -0300

    feat: Create findByEmail function for authentication use

--- src/app/repository/UserRepository.ts
+++ src/app/repository/UserRepository.ts
@@ -6,6 +6,10 @@ class UserRepository {
   async create (payload: IUser): Promise<IUserResponse> {
     return UserSchema.create(payload)
   }
+
+  async findByEmail (email: string): Promise<IUserResponse | null> {
+    return UserSchema.findOne({ email })
+  }
 }
 
 export default new UserRepository()
