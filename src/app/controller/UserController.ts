17df5affe12ce13020011dce549dd84d44066fa6
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-02 11:14:55 -0300

    feat: Create authenticate function on user controller

--- src/app/controller/UserController.ts
+++ src/app/controller/UserController.ts
@@ -1,4 +1,3 @@
-import { createTokenJWT } from '../utils/tokenCreation'
 import { IUser, IUserResponse } from '../interfaces/IUser'
 import UserService from '../service/UserService'
 
@@ -6,11 +5,25 @@ class UserController {
   async create (req, res): Promise<IUserResponse> {
     try {
       const payload: IUser = req.body
-      const token = createTokenJWT(payload)
-      res.setHeader('Authorization', token)
       const result = await UserService.create(payload)
 
       return res.status(201).json(result)
+    } catch (error) {
+      return res.status(error.statusCode || 500).json({
+        message: error.name,
+        details: [
+          { message: error.message }
+        ]
+      })
+    }
+  }
+
+  async authenticate (req, res): Promise<IUserResponse> {
+    try {
+      const { email, password }: IUser = req.body
+      const result = await UserService.authenticate(email, password)
+
+      return res.status(200).json(result)
     } catch (error) {
       return res.status(error.statusCode || 500).json({
         message: error.name,
