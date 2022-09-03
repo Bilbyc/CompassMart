921667642ef3cdc68b03a7e63e1bb0bbf311d649
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-03 13:41:28 -0300

    refactor: Add pagination settings to a proper file

---
+++ src/app/utils/pagination.ts
@@ -0,0 +1,12 @@
+export const myCustomLabels = {
+  totalDocs: 'total',
+  docs: 'Products',
+  limit: 'limit',
+  totalPages: 'offsets',
+  page: 'offset',
+  nextPage: false,
+  prevPage: false,
+  pagingCounter: false,
+  hasPrevPage: false,
+  hasNextPage: false
+}
