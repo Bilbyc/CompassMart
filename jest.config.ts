ebf956d52b5b8bd2af4726a1e4507bb66dc1ae17
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-10 13:10:09 -0300

    feat: Create a jest config file

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+
+export default {
+
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  bail: true,
+  clearMocks: true,
+  testMatch: [
+    // '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  setupFilesAfterEnv: ['./src/app/__tests__/databaseConnection.ts'],
+  collectCoverageFrom: ['src/**']
+}
