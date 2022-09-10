ebe1ef555ca1a6123bd672f3c2443ee22943b4f7
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-10 14:10:35 -0300

    feat: Create Product variable examples to be used on tests

---
+++ src/app/__tests__/Integration/ProductService.spec.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import App from '../../../app'
+import request from 'supertest'
+
+const server = request(App)
+
+jest.setTimeout(1000000)
+
+let token
+
+const testPassword = '123456'
+
+const testProduct = {
+  title: 'Refrigerante',
+  description: 'Coca Cola 600ml',
+  department: 'Depósitos',
+  brand: 'Coca cola',
+  price: 5.50,
+  qtd_stock: 866,
+  bar_codes: '1234567891011'
+}
+
+let productId
