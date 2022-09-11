fc60f524523fea0968956121e4417d6b92180b81
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-11 17:46:26 -0300

    fix: Logger errors shows up when running tests

--- src/app/__tests__/databaseConnection.ts
+++ src/app/__tests__/databaseConnection.ts
@@ -2,8 +2,10 @@
 import ProductSchema from '../schema/ProductSchema'
 import UserSchema from '../schema/UserSchema'
 import Database from '../../infra/database/mongo/index'
+import Logger from '../utils/loggers/winstonConfig'
 
 global.beforeAll(async () => {
+  Logger.silent = true
   await Database.connect()
   await ProductSchema.deleteMany({})
   await UserSchema.deleteMany({})
@@ -13,4 +15,5 @@ global.afterAll(async () => {
   await ProductSchema.deleteMany({})
   await UserSchema.deleteMany({})
   await Database.disconnect()
+  Logger.silent = false
 })
