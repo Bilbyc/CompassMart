fd92d47bc831950a97ea293086144ae459507f4e
Carlos Bilby <carlos.bilby@gmail.com>
2022-09-15 11:20:05 -0300

    refactor: Add logger to register csv file related errors

--- src/app/middlewares/csvValidator.ts
+++ src/app/middlewares/csvValidator.ts
@@ -1,9 +1,11 @@
+import Logger from '../utils/loggers/winstonConfig'
 import { Request, Response, NextFunction } from 'express'
 
 export const csvValidator = async (req: Request, res: Response, next: NextFunction) => {
   const { file } = req
 
   if (!file) {
+    Logger.error('[POST /api/v1/product/csv]: No file was sent')
     return res.status(400).json({
       message: 'Bad Request',
       details: [{
@@ -13,6 +15,7 @@ export const csvValidator = async (req: Request, res: Response, next: NextFuncti
   }
 
   if (file.mimetype !== 'text/csv') {
+    Logger.error('[POST /api/v1/product/csv]: File sent is not a CSV')
     return res.status(400).json({
       message: 'Bad Request',
       details: [{
